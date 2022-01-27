f3ad8611c20b4140c617878d6d3ff8dcacaa1582
mckayreedmoore <75471605+mckayreedmoore@users.noreply.github.com>
2022-01-26 18:10:15 -0700

    Homework update

--- basic-javascript-projects/project1-expressions-alert/js/main.js
+++ basic-javascript-projects/project1-expressions-alert/js/main.js
@@ -1,15 +1,17 @@
-var x = ("\"hello, world!\"") ;
+var x = ("\"hello, world!\"") ; // assigned "hello world" to variable x
 
-window.alert (x);    
+window.alert (x);    // gives a window alert of variable x, "hello world"
 
-document.write ("this is the day " + "where i concatenate a string " + "and do it using javascript" );
+document.write ("this is the day " + "where i concatenate a string " + "and do it using javascript" ); // writes contents of paranthesis on home page with the contactenations
 
-var y = ("          concatenated " + "string");
+var y = ("          concatenated " + "string"); // Demonstrates contactenation in assiging a variable a value. 
 
-document.write (y);
+document.write (y); //writes variable y on the page
 
-var jill = " girl", jack = " boy", hill = " geographical feature", well = " architectural immplement";
+var jill = " girl", jack = " boy", hill = " geographical feature", well = " architectural immplement"; // demonstrates assigning multiple variables on one line. 
 
-document.write (jill);
+document.write (jill); //writes one of the previously assigned variables
 
-window.alert (3 + 3);
+window.alert (3 + 3); //demonstrates simple mathmatics is possible when giving values. 
+
+document.write(y.fontcolor ("blue"));
