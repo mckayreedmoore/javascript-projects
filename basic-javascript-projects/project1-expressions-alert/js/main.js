09226b96bf2f5b42e3677f1fd592c0b8a4b928f3
mckayreedmoore <75471605+mckayreedmoore@users.noreply.github.com>
2022-01-26 18:00:18 -0700

    Homework updates

---
+++ basic-javascript-projects/project1-expressions-alert/js/main.js
@@ -0,0 +1,15 @@
+var x = ("\"hello, world!\"") ;
+
+window.alert (x);    
+
+document.write ("this is the day " + "where i concatenate a string " + "and do it using javascript" );
+
+var y = ("          concatenated " + "string");
+
+document.write (y);
+
+var jill = " girl", jack = " boy", hill = " geographical feature", well = " architectural immplement";
+
+document.write (jill);
+
+window.alert (3 + 3);
