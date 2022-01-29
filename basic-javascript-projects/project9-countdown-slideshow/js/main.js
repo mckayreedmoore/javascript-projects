26a7bc236e37bdaca62af6eb74b71e87472466f8
mckayreedmoore <75471605+mckayreedmoore@users.noreply.github.com>
2022-01-29 02:58:12 -0700

    updated homework

---
+++ basic-javascript-projects/project9-countdown-slideshow/js/main.js
@@ -0,0 +1,45 @@
+function countdowntimer () {
+    var seconds = document.getElementById("seconds").value;
+    function tick(){
+        seconds = seconds - 1;
+        timer.innerHTML = seconds;
+        var time = setTimeout(tick, 1000);
+        if (seconds == -1) {
+            alert("times up!");
+            clearTimeout(time);
+            timer.innerHTML = "";
+        }
+    }
+    tick();
+}
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideInde-1].className += "active";
+    
+}
